9a676dfab6ecd9ea2d5c92a12767b65645951615
samuelzapatam22 <samuelzapatam1@gmail.com>
2023-12-20 14:18:57 -0500

    comentando index.js para comprension

--- reto en clase/index.js
+++ reto en clase/index.js
@@ -1,41 +1,28 @@
+// Función asincrónica para obtener datos del clima
 async function fetchWeather() {
+    // Obtener el nombre de la ciudad desde el campo de entrada en el formulario
     const city = document.getElementById("city").value;
+    // Obtener el contenedor para mostrar la información del clima
     const weatherInfoContainer = document.getElementById("weather-info");
 
     try {
-        const apiKey = '1c78da2e186c63f952450c9b21bc8a5b'; // Reemplaza con tu clave API real de OpenWeatherMap
+        // Clave de la API para acceder a OpenWeatherMap
+        const apiKey = '1c78da2e186c63f952450c9b21bc8a5b';
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+        // Realizar una solicitud a la API de OpenWeatherMap para obtener datos del clima
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}');
         const weatherData = await response.json();
 
-        // Display weather information
+        // Mostrar la información del clima en el contenedor
         weatherInfoContainer.innerHTML = generateWeatherHTML(weatherData);
     } catch (error) {
+        // Manejar errores y mostrar un mensaje de error en caso de falla
         console.error("Error fetching weather data:", error);
-        // Display error message
         weatherInfoContainer.innerHTML = "<p>Error fetching weather data. Please try again.</p>";
     }
 }
 
-async function fetchWeather() {
-    const city = document.getElementById("city").value;
-    const weatherInfoContainer = document.getElementById("weather-info");
-
-    try {
-        const apiKey = '1c78da2e186c63f952450c9b21bc8a5b'; // Reemplaza con tu clave API real de OpenWeatherMap
-
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-        const weatherData = await response.json();
-
-        // Display weather information
-        weatherInfoContainer.innerHTML = generateWeatherHTML(weatherData);
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        // Display error message
-        weatherInfoContainer.innerHTML = "<p>Error fetching weather data. Please try again.</p>";
-    }
-}
-
+// Función para generar el HTML que muestra la información del clima
 function generateWeatherHTML(weatherData) {
     return `
         <h2>Weather Information for ${weatherData.name}, ${weatherData.sys.country}</h2>
