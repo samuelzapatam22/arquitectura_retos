22f6232f5260fc1a5661babc5dc3851bf51c40c2
samuelzapatam22 <samuelzapatam1@gmail.com>
2023-12-20 13:12:15 -0500

    endpoint get en app.js

--- reto en clase/app.js
+++ reto en clase/app.js
@@ -15,19 +15,4 @@ app.get('/api/weather/:city', (req, res) => {
 });
 
 // Ruta para el endpoint /weather
-app.post('/weather', (req, res) => {
-    const city = req.body.city;
-    const weatherData = getMockWeatherData(city);
-    res.send(weatherData);
-});
 
-// Función de simulación de datos para la API del clima
-function getMockWeatherData(city) {
-    return {
-        city: city,
-        temperature: Math.floor(Math.random() * 30) + 1,
-        humidity: Math.floor(Math.random() * 100) + 1,
-        pressure: Math.floor(Math.random() * 1000) + 1,
-        wind: Math.floor(Math.random() * 20) + 1
-    };
-}
